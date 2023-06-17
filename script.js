546a0c1dd8d8d53dbbf7152a6208332663b72d85
ckontos <har.kontos@hotmail.com>
2023-06-18 01:48:58 +0300

    V1.8

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     var menu = document.querySelector('.hamburger-menu');
     var leftColumn = document.querySelector('.left-column');
-
+    var menuLines = document.querySelectorAll('.hamburger-menu div');
+  
     // Initially hide the left column
     leftColumn.classList.remove("show");
-
+  
     menu.addEventListener('click', function() {
-        leftColumn.classList.toggle('show');
+      leftColumn.classList.toggle('show');
+      menuLines.forEach(function(line) {
+        line.classList.toggle('change');
+      });
     });
-});
+  });
+  
